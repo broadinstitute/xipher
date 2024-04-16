8a7046499015292d51d3ac62609cb6e24c28c8f4
Alec Wysoker <alecw@broadinstitute.org>
2024-04-16 16:28:55 -0400

    Script to prepare gnomAD variants for xipher

---
+++ src/R/packages/xipher/R/prepare_gnomAD.R
@@ -0,0 +1,23 @@
+#' Prepare gnomAD variants table for xipher::prepareVcfClp
+#' @param outGnomADPath path to write gnomAD variants table.  You can then pass this to prepareVcfClp() with gnomAdPath=
+#' @param inGnomADVariantsTablePath path to gnomAD variants table produced by GATK VariantsToTable
+#' @param maf.min minimum allele frequency
+#' @export
+#' @import data.table
+prepareGnomAClp<-function(outGnomADPath,
+                          inGnomADVariantsTablePath,
+                          maf.min = 0.001) {
+  gnomad <- data.table::fread(inGnomADVariantsTablePath)
+
+  colnames( gnomad ) = tolower( colnames( gnomad ) )
+  
+  gnomad[ , maf := min( af, 1 - af ), by = 1:nrow( gnomad ) ]
+  gnomad = gnomad[ maf >= maf.min ]
+  
+  gnomad = gnomad[ type == "SNP" ]
+  
+  gnomad = gnomad[ chrom == "X" | chrom == "chrX" ]
+  
+  setcolorder( gnomad, c( "chrom", "pos", "ref", "alt", "type", "ac", "an", "af", "maf" ) )
+  write_table_helper(outGnomADPath, gnomad)
+}
