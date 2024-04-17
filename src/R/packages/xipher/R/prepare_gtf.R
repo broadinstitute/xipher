9b1d1e345a222dc2519fe528ecfcabaf1f4c9949
Alec Wysoker <alecw@broadinstitute.org>
2024-04-17 10:47:22 -0400

    Enable markdown and document default X contig name

--- src/R/packages/xipher/R/prepare_gtf.R
+++ src/R/packages/xipher/R/prepare_gtf.R
@@ -3,10 +3,9 @@
 #' @param inReducedGtfPath path to reduced GTF file produced by ReduceGtf (see https://github.com/broadinstitute/Drop-seq/).
 #'                         Note that you can speed ReduceGtf and make it's output smaller by providing a sequence dictionary
 #'                         that contains only the X contig.
-#' @param X_contig_name name of the X contig
+#' @param X_contig_name name of the X contig. Default: `r toString(default_X_contig_name)`
 #' @export
 #' @import data.table
-#' @seealso [xipher::default_X_contig_name]
 prepareGtfClp<-function(outAnnotationsPath,
                         inReducedGtfPath,
                         X_contig_name=default_X_contig_name) {
